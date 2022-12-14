40edf8c1a889da9b8ac5df65e0e866c5153ffcbe
jangheejin <sl150s@naver.com>
2022-12-14 14:33:35 +0900

    LoginPage.js 로그인 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import { NavLink, Route, withRouter } from "react-router-dom";
-import { Home, Study, Game, Buy } from "./pages";
+import { Home, Study, Game, Buy, LoginPage } from "./pages";
 
 
 function App({ location, match, history }) {
@@ -30,10 +30,13 @@ function App({ location, match, history }) {
         <li><NavLink activeStyle={activeStyle} to="/buy?code=1&name=handphone">1번 상품 구매</NavLink></li>
         <li><NavLink activeStyle={activeStyle} to="/buy?code=2&name=water">2번 상품 구매</NavLink></li>
         <li><NavLink activeStyle={activeStyle} to="/buy?code=3&name=coffee">3번 상품 구매</NavLink></li>
+        <li><NavLink activeStyle={activeStyle} to="/login">3번 상품 구매</NavLink></li>
+      
       </ul>
       <Route exact path="/" component={Home} />
       <Route exact path="/study" component={Study} />
       <Route exact path="/game" component={Game} />
+      <Route exact path="/login" component={LoginPage} />
 
       {/* name이라는 이름의 경로 파라미터를 읽을수 있도록 설정 */}
       <Route path="/game/:name" component={Game} />
